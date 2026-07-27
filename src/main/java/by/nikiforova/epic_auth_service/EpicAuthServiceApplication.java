71f4264aeb0ac1ce11f39f641920f5373c7943a9
Katsia-Niki <katsiaryna.niki@gmail.com>
2026-07-27 08:59:45 +0300

    First commit

---
+++ src/main/java/by/nikiforova/epic_auth_service/EpicAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package by.nikiforova.epic_auth_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpicAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpicAuthServiceApplication.class, args);
+	}
+
+}
